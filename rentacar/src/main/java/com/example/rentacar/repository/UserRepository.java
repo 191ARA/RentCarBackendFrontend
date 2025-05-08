d11ae51c06ba56571ecf8eaccd8cfd4ac190e832
Arman <arman-sokl007@mail.ru>
2025-05-08 23:52:32 +0500

    Delete rentacar directory

--- rentacar/src/main/java/com/example/rentacar/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.rentacar.repository;
-
-import com.example.rentacar.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
